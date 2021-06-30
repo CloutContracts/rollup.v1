c08dfc1833afe01d6121e6cb14c4465a5d044571
Andrew Kamal <kamalandrew55@gmail.com>
2021-06-29 22:58:41 -0400

    Update and rename TestToken.sol to Token.sol

--- code/Solidity/Token.sol
+++ code/Solidity/Token.sol
@@ -4,9 +4,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/ownership/Ownable.sol";
 
 /**
- * @title TestToken is a basic ERC20 Token
+ * @title CloutContracts is a basic ERC20 Token
  */
-contract TestToken is ERC20, Ownable {
+contract CloutContracts is ERC20, Ownable {
     /**
      * @dev assign totalSupply to account creating this contract */
     constructor() public {
